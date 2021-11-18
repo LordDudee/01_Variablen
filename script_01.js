e9dee80615a9472579ccb8eb03c274fd2586972f
Michael <michael_runschke@gmx.de>
2021-11-18 11:17:57 +0100

    Deklaration + Zuweisung

--- script_01.js
+++ script_01.js
@@ -1,2 +1,38 @@
 /***** Variablen 01 *******/
 
+/**
+ * Block-Kommentar
+ */
+
+// Zeilen-Kommentar
+
+// Konsolen-Ausgabe
+// console.log("Hello, World!");
+// console.log(firstName); // deklarierte Variante ---> Fehler
+
+/**** 02 Deklaration + Wertzuweisung *******/
+// 1.Möglichkeit
+// let firstName = "Max"; // Deklaration (Definition) + Wertzuwesiung
+// console.log(firstName);
+
+// 2.Möglichkeit
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuwesiung
+// console.log(firstName); // Ausgabe
+
+// let familyName = "Mütze"; // Deklaration (Definition) + Wertzuwesiung
+// console.log(familyName); // Ausgabe
+// console.log(firstName + " " + familyName);
+
+/***** 03 Deklaration + Wertzuwesiung II *******/
+
+// let firstName, familyName;
+// firstName = prompt("Bitte Vornamen eingeben");
+// familyName = prompt("Bitte Nachnamen eingeben");
+// console.log(firstName + " " + familyName);
+
+let test;
+test = "moin"
+test = 2;
+test = true;
+console.log("Inhalt: " + test);
